70fe2faa88546bdd92c6a4a8d1a3d9c5f1846ef0
Shirley Michaely <shirley.michaely@gmail.com>
2018-05-27 16:57:46 +0300

    fix(visuals-alignment): now transform settings work dynamically for both flipped and regular visualizations

--- client/App/Visuals/VizualEffect.js
+++ client/App/Visuals/VizualEffect.js
@@ -1,9 +1,7 @@
 import React from 'react';
 
-// TODO: change from hard coded pixels position to relative centered position
 export default ({ id, opacity, borderRadius, visable, flipped }) => <canvas className='centered' style={{
   opacity: visable ? opacity : 0, 
   borderRadius, 
   zIndex: 1,
-transform: flipped ? 'scale(-1)' : 'scaleY(1)',
-left:'362px'}} id={id} />;
+transform: flipped ? 'scale(-1) translate(50%, 0)' : 'translate(-50%, 0'}} id={id} />;
